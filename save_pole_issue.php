6f1cf283a7fbf4ad67d375ebac8ba68f5c66bc26
cusinbs <cusinbs@gmail.com>
2024-02-06 14:24:48 +0000

    Change to use SMTP instead of HTML mail

--- save_pole_issue.php
+++ save_pole_issue.php
@@ -5,6 +5,11 @@
     </body>
 </html>
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require './PHPMailer/src/Exception.php';
+    require './PHPMailer/src/PHPMailer.php';
+    require './PHPMailer/src/SMTP.php';
 /* ************************ NOTE **********************************/
 /* This save pole issue use QR code page */
 $poleid = $_GET['poleid'];
@@ -37,7 +42,8 @@ if(!$result) {
 }
 echo "<div style='font-size:75px ;color:green'>Update pole issue report successed</div>";
 file_put_contents("Log.txt", date('H:i:s m/d/Y',$now)."Entering send mail from save pole issue\n", FILE_APPEND | LOCK_EX);
-sendemail($issue,$contact,$poleid,$subdiv);
+//sendemail($issue,$contact,$poleid,$subdiv);
+sendsmtp($issue,$contact,$poleid,$subdiv);
 
 $conn->close();
 
@@ -75,6 +81,39 @@ function sendemail($issue,$contact,$poleid,$subdiv){
         //echo 'Unable to send email. Please try again.';
 	file_put_contents("Log.txt", date('H:i:s m/d/Y',$now)."Poleissue: Sending mail failed\n", FILE_APPEND | LOCK_EX);
         http_response_code(503);
+    }
+}
+function sendsmtp($issue,$contact,$poleid,$subdiv) {
+    $mail = new PHPMailer;
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.hostinger.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'support@de1fl.com';                 // SMTP username
+    $mail->Password = 'An940822?';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->From = 'support@de1fl.com';
+    $mail->FromName = 'Support';
+    //$mail->addAddress('Tony.Toniolo@duke-energy.com', 'Tony Toniolo');     // Add a recipient
+    //$mail->addAddress('Tony.Toniolo@duke-energy.com');       // Name is optional
+    $mail->addAddress('ghecu@hotmail.com', 'Giac Dinh');     // Add a recipient
+    $mail->addAddress('ghecu@hotmail.com');       // Name is optional
+    $mail->addReplyTo('support@de1fl.com', 'support@de1fl.com');
+    //$mail->addCC('Gerald.Rooks@duke-energy.com');
+    $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Email send from Support@DE1FL.COM';
+    $message = '<html><body>';
+    $message .= '<p style="color:#3498DB;font-size:18px;">Subdivision:' . $subdiv . '</p>';
+    $message .= '<p style="color:#3498DB;font-size:18px;">PoleID:' . $poleid . '</p>';
+    $message .= '<p style="color:#3498DB;font-size:18px;">Issue:' . $issue . '</p>';
+    $message .= '<p style="color:#3498DB;font-size:18px;">Contact:' . $contact. '</p>';
+    $message .= '</body></html>';
+    $mail->Body    = $message;
+    if(!$mail->send()) {
+	file_put_contents("Log.txt", date('H:i:s m/d/Y',$now)." ".$mail->ErrorInfo." SMTP: Sending mail failed\n", FILE_APPEND | LOCK_EX);
+    } else {
+	file_put_contents("Log.txt", date('H:i:s m/d/Y',$now)."SMTP: Sending mail passed\n", FILE_APPEND | LOCK_EX);
     }
 }
 
